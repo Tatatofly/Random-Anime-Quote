c84c3f0367d10d3a94ccf0620ccae54b05b7214f
Tatu Toikkanen <tatatofly@gmail.com>
2019-03-12 20:25:44 +0200

    front

---
+++ build/precache-manifest.f9d79f7f723cff36fee4007fd6a3057b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e29b1d8a631b30a75376",
+    "url": "/static/js/main.2ac449cd.chunk.js"
+  },
+  {
+    "revision": "5801bfebcf52011fae3c",
+    "url": "/static/js/2.188207e2.chunk.js"
+  },
+  {
+    "revision": "e29b1d8a631b30a75376",
+    "url": "/static/css/main.0f116fda.chunk.css"
+  },
+  {
+    "revision": "ad684fa8fb7d677ae4cd08f943f180a3",
+    "url": "/index.html"
+  }
+];
